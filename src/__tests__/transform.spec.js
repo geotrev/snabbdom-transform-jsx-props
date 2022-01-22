eb1364390b119c9f736b6810b925489ce5961ff7
George Treviranus <geowtrev@gmail.com>
2022-01-22 15:09:43 -0800

    test: adds className prop test

--- src/__tests__/transform.spec.js
+++ src/__tests__/transform.spec.js
@@ -1,10 +1,14 @@
 import { jsx } from "snabbdom"
-import "../transform.js"
+import { transform } from "../transform.js"
 
 /* eslint-disable jest/expect-expect */
 
 describe("transform", () => {
-  it("applies className prop to data.props.className", () => {})
+  it("applies className prop to data.props.className", () => {
+    const state = transform(<div className="foo" />)
+    expect(state.data.className).toBe(undefined)
+    expect(state.data.props.className).toBe("foo")
+  })
   it("applies aria prop to data.attrs", () => {})
   it("applies data prop to data.dataset", () => {})
   it("applies event handler data.on", () => {})
