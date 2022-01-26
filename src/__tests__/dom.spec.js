97c0f1387d821fb0a1229dcc4f4ed8db9e2f6f8a
George Treviranus <geowtrev@gmail.com>
2022-01-25 19:25:36 -0800

    test: adds additional tests for not regressing handling of snabbdom module props

--- src/__tests__/dom.spec.js
+++ src/__tests__/dom.spec.js
@@ -20,73 +20,119 @@ const patch = init([
   datasetModule,
 ])
 
-function mount(vnode, nextVnode) {
-  const state = patch(vnode, transform(nextVnode))
-  document.body.appendChild(state.elm)
-  return state.elm
+function mount(baseNode, nextVnode) {
+  const state = patch(baseNode, transform(nextVnode))
+  const wrapper = document.createElement("div")
+  wrapper.id = "root"
+  wrapper.appendChild(state.elm)
+  document.body.appendChild(wrapper)
+  return state
 }
 
 describe("DOM", () => {
-  let vnode
+  let baseNode
 
   beforeEach(() => {
-    vnode = toVNode(document.createElement("div"))
+    baseNode = toVNode(document.createElement("div"))
   })
-  
+
   afterEach(() => {
-    vnode = null
-    document.body.removeChild(document.body.firstElementChild)
+    baseNode = null
+    const wrapper = document.getElementById("root")
+    if (wrapper) wrapper.parentNode.removeChild(wrapper)
   })
 
-  describe("attr prefix", () => {
-    it("sets type attribute with 'attr-' prefix", () => {
-      const fixture = mount(vnode, <button attr-type='button' />)
-      expect(fixture.getAttribute("type")).toEqual("button")
+  describe("prefixes", () => {
+    it("sets with 'attr-' prefix", () => {
+      const { elm } = mount(baseNode, <button attr-type="button" />)
+      expect(elm.getAttribute("type")).toEqual("button")
+    })
+
+    it("sets with prop- prefix", () => {
+      const { elm } = mount(baseNode, <button prop-type="button" />)
+      expect(elm.type).toEqual("button")
     })
   })
 
-  describe("prop prefix", () => {
-    it("sets to html dom property", () => {
-      const fixture = mount(vnode, <button attr-type='button' />)
-      expect(fixture.getAttribute("type")).toEqual("button")
-    })
-  })
-
-  describe("dom property props", () => {
-    it("sets className to className property", () => {
-      const fixture = mount(vnode, <button className="test" />)
-      expect(fixture.getAttribute("class")).toEqual("test")
-    })
-    it("sets class-name to className property", () => {
-      const fixture = mount(vnode, <button class-name="test" />)
-      expect(fixture.getAttribute("class")).toEqual("test")
-    })
-    it("sets id to id property", () => {
-      const fixture = mount(vnode, <button id="test" />)
-      expect(fixture.id).toEqual("test")
-    })
-    it("sets tabIndex to tabIndex property", () => {
-      const fixture = mount(vnode, <button tabIndex={0} />)
-      expect(fixture.tabIndex).toEqual(0)
-    })
-    it("sets tab-index to tabIndex property", () => {
-      const fixture = mount(vnode, <button tab-index={0} />)
-      expect(fixture.tabIndex).toEqual(0)
-    })
-  })
-  
   describe("dataset props", () => {
     it("sets to dataset and data attribute", () => {
-      const fixture = mount(vnode, <button data-foo-bar="baz" />)
-      expect(fixture.dataset.fooBar).toEqual("baz")
-      expect(fixture.getAttribute("data-foo-bar")).toEqual("baz")
+      const { elm } = mount(baseNode, <button data-foo-bar="baz" />)
+      expect(elm.dataset.fooBar).toEqual("baz")
+      expect(elm.getAttribute("data-foo-bar")).toEqual("baz")
     })
   })
 
   describe("generic props", () => {
     it("sets attribute to element", () => {
-      const fixture = mount(vnode, <button aria-label="test" />)
-      expect(fixture.getAttribute("aria-label")).toEqual("test")
+      const { elm } = mount(baseNode, <button aria-label="test" />)
+      expect(elm.getAttribute("aria-label")).toEqual("test")
+    })
+  })
+
+  describe("aliased props", () => {
+    it("sets className to className property", () => {
+      const { elm } = mount(baseNode, <button className="test" />)
+      expect(elm.getAttribute("class")).toEqual("test")
+    })
+
+    it("sets class-name to className property", () => {
+      const { elm } = mount(baseNode, <button class-name="test" />)
+      expect(elm.getAttribute("class")).toEqual("test")
+    })
+
+    it("sets id to id property", () => {
+      const { elm } = mount(baseNode, <button id="test" />)
+      expect(elm.id).toEqual("test")
+    })
+
+    it("sets tabIndex to tabIndex property", () => {
+      const { elm } = mount(baseNode, <button tabIndex={0} />)
+      expect(elm.tabIndex).toEqual(0)
+    })
+
+    it("sets tab-index to tabIndex property", () => {
+      const { elm } = mount(baseNode, <button tab-index={0} />)
+      expect(elm.tabIndex).toEqual(0)
+    })
+  })
+
+  describe("modules", () => {
+    it("does not regress: event listeners", () => {
+      const handler = jest.fn()
+      const { elm } = mount(baseNode, <button on={{ click: handler }} />)
+      elm.click()
+      expect(handler).toBeCalled()
+    })
+
+    it("does not regress: class", () => {
+      const { elm } = mount(baseNode, <button class={{ test: true }} />)
+      expect(elm.classList.contains("test")).toBe(true)
+    })
+
+    it("does not regress: props", () => {
+      const { elm } = mount(baseNode, <button props={{ tabIndex: 0 }} />)
+      expect(elm.tabIndex).toEqual(0)
+    })
+
+    it("does not regress: style", () => {
+      const { elm } = mount(baseNode, <button style={{ fontWeight: "bold" }} />)
+      expect(elm.style.fontWeight).toEqual("bold")
+    })
+
+    it("does not regress: dataset", () => {
+      const { elm } = mount(baseNode, <button dataset={{ fooBar: "baz" }} />)
+      expect(elm.dataset.fooBar).toEqual("baz")
+    })
+
+    it("does not regress: attributes", () => {
+      const { elm } = mount(baseNode, <button attrs={{ tabindex: "0" }} />)
+      expect(elm.getAttribute("tabindex")).toEqual("0")
+    })
+
+    it("does not regress: hooks", () => {
+      const handler = jest.fn()
+      mount(baseNode, <button hook={{ insert: handler }} />)
+      expect(handler).toBeCalled()
     })
   })
 })
